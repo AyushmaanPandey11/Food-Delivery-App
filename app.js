63ea778d816723246a2d184830442c1c997a3902
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-01-23 23:21:48 +0530

    Body Section

--- app.js
+++ app.js
@@ -6,14 +6,44 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-                <img className="logo" src="applogo.jpg" alt="logo" />
+                <img className="logo" src="https://w7.pngwing.com/pngs/894/279/png-transparent-online-food-ordering-food-delivery-grubhub-others-food-service-logo-thumbnail.png" alt="logo Not Coming"/>
             </div>
             <div className="Nav-Bar" >
                 <ul>
                     <li>Home</li>
-                    <li>About</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
                     <li>Cart</li>
                 </ul>
+            </div>
+        </div>
+    );
+};
+
+const Restaurentcard = (prop) => {
+    return (
+
+        <div className="res-card" >
+            <img className="res-logo" alt="restaurent logo" 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3sbDbh1CFfkBLFCL2XWW2M84Elff1wX40a-koRbhKEA&s"/> 
+            <h3>{prop.resname}</h3>
+            <h4>{prop.cuisine}</h4>
+            <h4>Rating: {prop.rating}</h4>
+            <h4>Time: {prop.deliverytime}</h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body" >
+            <div className="search" >Search Bar</div>
+            <div className="res-container" >
+                <Restaurentcard resname="Noodle House" cuisine="North-East, Momos,Fried Rice" rating="4.9" deliverytime="40 mins" />
+                <Restaurentcard resname="Sangrila" cuisine="North-East, Momos,Fried Rice" rating="5.0" deliverytime="30 mins"/>
+                <Restaurentcard/>
+                <Restaurentcard/>
+                <Restaurentcard/>
             </div>
         </div>
     );
@@ -25,6 +55,7 @@ const AppLayout = () =>
     return (
         <div className="app">
             <Header/>
+            <Body />
         </div>
     );
 };
