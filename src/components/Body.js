0fa091f5ff7540d2deea597f458e6dfa499c6b8e
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-06 03:24:08 +0530

    IntroducedReactHooks(useStae)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,24 @@
 import Restaurentcard  from "./Restaurentcard";
+import { useState } from "react";
 import cardobj from "../utils/mockData";
 const Body = () => {
+  const [listofRes, setlistofRes] = useState(cardobj);
     return (
         <div className="body" >
             <div className="search" >Search Bar</div>
+            <div className="filter" >
+              <button className="filter-btn" 
+                onClick={ () => {
+                  const filteredList = listofRes.filter(
+                  (res) => res.info.avgRating > 4.5 );
+                  setlistofRes(filteredList) 
+                } }
+              >
+                Top Restaurents
+              </button>
+            </div>
             <div className="res-container" >
-                { cardobj.map((restaurent) => (
+                { listofRes.map((restaurent) => (
                   <Restaurentcard key = { restaurent.info.id} resdata={restaurent}/>
                 )) }
             </div>
