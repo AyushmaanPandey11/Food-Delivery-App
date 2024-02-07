210ad192e7544cb62e8de368b3c56850d83d588a
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-07 18:06:33 +0530

    IntroducedSearchOptionAndLoginLogOutOption

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,15 @@
 import Restaurantcard  from "./Restaurantcard";
 import { useEffect,useState} from "react";
-import cardobj from "../utils/mockData";
+//import cardobj from "../utils/mockData";
+import Shimmer from "./Shimmer";
 const Body = () => {
-    const [originalList,setoriginalList] = useState(cardobj); // just to store the original values 
+    const [originalList,setoriginalList] = useState([]); // just to store the original values 
     const [filteredlistofRes, setfilteredlistofRes] = useState(originalList); // used to in functions as it will keep changin afte events
-    
+    const [searchText, setsearchText] = useState([]);
     useEffect( ()=>{fetchdata()} ,[]);
     const fetchdata = async () => {
       const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4399295&lng=78.4982741&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
       const json = await data.json();
-      console.log(json);
       setfilteredlistofRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
       setoriginalList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
@@ -18,17 +18,28 @@ const Body = () => {
     }
     const filterTopRestaurant = () => {
       const filteredList = originalList.filter(
-      (res) => res.info.avgRating >= 4.5 );
+      (res) => res.info.avgRating >= 3.5 );
       setfilteredlistofRes(filteredList);
     }
-    return (
+    const searchType = (e) => {
+      setsearchText(e.target.value);
+    } 
+    const searchResult = () =>{
+      const searchList = originalList.filter( (res) => res.info.name.toLowerCase().includes( searchText.toLowerCase() ) 
+      )
+      setfilteredlistofRes(searchList);
+    }
+    return  filteredlistofRes.length === 0 ? (<Shimmer />) : (
         <div className="body" >
-            <div className="search" >Search Bar</div>
+            <div className="search" >
+              <input className="search-box" value={searchText} onChange={searchType} ></input>
+              <button className="search-btn" onClick={searchResult}>Search</button>
+            </div>
             <div className="filter" >
               <button className="filter-btn" 
                 onClick={ filterTopRestaurant }
               >
-                Top Restaurants
+                Top Rated Restaurants
               </button>
               <button className="filter-btn" onClick={resetList} >
                 Home
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 // header section 
 const Header = () => {
+    const [ btn, setbtn ] = useState(["Login"]);
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -13,6 +16,9 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login-btn" onClick={ () => {
+                        btn === "Login" ? setbtn("Logout") : setbtn("Login");
+                    }} >{btn}</button>
                 </ul>
             </div>
         </div>
