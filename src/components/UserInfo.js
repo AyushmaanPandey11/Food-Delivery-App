f764f04bab2fc57a13d9a24712e86a8e4d8d7df6
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-15 19:05:22 +0530

    ComponentDidMount() using Api

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -12,23 +12,25 @@ class UserInfo extends React.Component{
         };
         
     } 
-    // async componentDidMount(){
-    //     const data = await fetch("https://api.github.com/users/AyushmaanPandey11");
-    //     const json = await data.json();
-    //     this.setState({
-    //         UserDetails:json,
-    //     })
-    // }
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/AyushmaanPandey11");
+        const json = await data.json();
+        this.setState({
+            UserDetails:json,
+        })
+    }
     render()
     {
 
         const {count} = this.state;
-        const {name, mail, location} = this.props;
+        const { login,avatar_url, name,bio  } = this.state.UserDetails;
         return (
             <div className="User-info-container" >
+                <h1>Data Fetching Using Api call</h1>
+                <img src={avatar_url} />
                 <h1>Name : {name}</h1>
-                <h2>Email id : {mail}</h2>
-                <h3>Location : {location}</h3>
+                <h2>UserName : {login}</h2>
+                <h3>Bio : {bio}</h3>
                 <h3> Count: {count} </h3>
                 <button onClick={ ()=> {
                     this.setState(
