2905ef0811efab5c63b26b4128cf6afa7f7c8f32
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-23 23:59:30 +0530

    Higher Order Component for Restaurant deliverying in 35 mins

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import Restaurantcard  from "./Restaurantcard";
+import Restaurantcard, {WithQuickDelivery}  from "./Restaurantcard";
 import Foodcarousel from "./Foodcarousel";
 import { useState} from "react";
 import Shimmer from "./Shimmer";
@@ -10,6 +10,7 @@ const Body = () => {
     const [originalList,setoriginalList,filteredlistofRes, setfilteredlistofRes] = useListofRes();
     const [foodcarousel,setfoodcarousel] = useFoodCarousel();
     const [searchText, setsearchText] = useState('');
+    const QuickDelRestaurant = WithQuickDelivery(Restaurantcard);
     // reset button 
     const resetList = () => {
       setfilteredlistofRes(originalList);
@@ -33,7 +34,7 @@ const Body = () => {
         <div className="body" >
             <div className="mx-5" >
               <input className="border-solid border-black border h-7 mr-3" value={searchText} onChange={searchType} ></input>
-              <button className="search-btn border-solid w-20 h-7 text-center mt-6 " onClick={searchResult}>Search</button>
+              <button className="search-btn border-solid w-20 h-7 text-center mt-6 bg-red-400" onClick={searchResult}>Search</button>
             </div>
             <div className=" m-5" >
               <button className="border-3 w-40 h-7 bg-yellow-500 mr-6 rounded-sm" 
@@ -56,19 +57,16 @@ const Body = () => {
               </div>
             </div>
             <div className="mx-[150px] " >
-              <h2>Restaurants Nearby..</h2>
+              <h2 className="text-xl text-black solid my-[10px]">Restaurants Nearby..</h2>
               <div className=" flex flex-wrap ">
                 { 
                   filteredlistofRes.map((restaurant) => (
-                  <Link key={restaurant?.info?.id} to={"/restaurants/"+restaurant?.info?.id}><Restaurantcard  resdata={restaurant}/></Link>
+                  <Link key={restaurant?.info?.id} to={"/restaurants/"+restaurant?.info?.id}>
+                    {restaurant?.info?.sla?.deliveryTime <= 35 ? <QuickDelRestaurant resdata={restaurant}/> : <Restaurantcard  resdata={restaurant}/>}
+                    </Link>
                   ))
                 }   
               </div>
-            </div>
-            <div className="MoreRes-btn">
-              <button  > 
-                More
-              </button>
             </div>
         </div>
     );
--- src/components/Restaurantcard.js
+++ src/components/Restaurantcard.js
@@ -4,17 +4,32 @@ const Restaurentcard = (prop) => {
     const { resdata } = prop;
     const { cloudinaryImageId, name, cuisines, avgRating,costForTwo,locality } = resdata?.info;
       return (
-          <div className=" shadow-lg m-5 py-4 px-3 h-[450px] w-[270px] bg-gray-300" >
+          <div className=" shadow-lg m-5 py-4 px-3 h-[550px] w-[270px] bg-gray-300 flex-wrap" >
                 <img className="rounded-md border-solid border-black w-[250] h-[250]" alt="restaurent logo" 
                   src={ CDN_URL + cloudinaryImageId}/> 
-                <div className="flex flex-wrap my-2 py-2 text-lg">
-                    <h3 className="res-details" >{name}</h3>
-                    <h4 className="res-details">{"Cuisines: "+ cuisines.join(", ")}</h4>
-                    <h4 className="res-details">{locality}</h4>
-                    <h4 className="res-details">Rating: {avgRating}</h4>
-                    <h4 className="res-details">costForTwo: {costForTwo}</h4>
+                <div className="flex flex-wrap my-2 py-2 text-base ">
+                    <h3 className="py-[5px] text-xl solid " >{name}</h3>
+                    <h4 className="py-[5px]">{"Cuisines: "+ cuisines.join(", ")}</h4>
+                    <h4 className="py-[5px]">{locality}</h4>
+                    <h4 className="py-[5px]">Rating: {avgRating}</h4>
+                    <h4 className="py-[5px]">costForTwo: {costForTwo}</h4>
                 </div>
           </div>
       );
   };
+
+// Higher Order Component for quick delivery(below 30mins)
+
+export const WithQuickDelivery = (Restaurantcard) => {
+  //return a functional component
+  return (props) => {
+    return (
+      <div>
+        <label className=" absolute bg-black text-white m-2 p-2 rounded-lg ml-2">Fast delivery</label>
+        <Restaurantcard  {...props} />
+      </div>
+    );
+  }
+}
+
 export default Restaurentcard;
