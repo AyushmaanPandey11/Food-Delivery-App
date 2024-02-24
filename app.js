33629cdbcac6bc459d5680d04cea652d3845c597
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-25 01:56:03 +0530

    Updating Context User Provider

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
@@ -8,17 +8,26 @@ import RestaurantMenu from "./src/components/RestaurantMenu";
 import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom"; 
 import { lazy,Suspense } from "react";
 import Shimmer from "./src/components/Shimmer";
-
+import UserContext from "./src/utils/UserContext";
 const Grocery = lazy( () => import("./src/components/Grocery") );
 const About = lazy( () => import("./src/components/About") );
 // main div tag 
 const AppLayout = () =>
 {
+    const [userName,setUserName]=useState();
+    useEffect(() => {
+        const data = {
+            name: "Pandey"
+        };
+        setUserName(data.name);
+    },[]);
     return (
+        <UserContext.Provider value={ {LoggedInUser : userName} } >
         <div className="app">
             <Header/>
             <Outlet />
         </div>
+        </UserContext.Provider>
     );
 };
 const AppRouter = createBrowserRouter( [
