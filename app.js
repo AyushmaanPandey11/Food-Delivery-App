13b3c731d909120073826364ee422dc42f7d474c
Ayushmaan Pandey <22352015@pondiuni.ac.in>
2024-02-27 23:58:00 +0530

    Updated Api

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
-import Body from "./src/components/Body";
+//import Body from "./src/components/Body";
 import Contact from "./src/components/Contact";
 import  Error  from "./src/components/Error";
-import RestaurantMenu from "./src/components/RestaurantMenu";
+//import RestaurantMenu from "./src/components/RestaurantMenu";
 import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom"; 
 import { lazy,Suspense } from "react";
 import Shimmer from "./src/components/Shimmer";
 import UserContext from "./src/utils/UserContext";
 const Grocery = lazy( () => import("./src/components/Grocery") );
+const Body = lazy( () => import("./src/components/Body") );
 const About = lazy( () => import("./src/components/About") );
+const RestaurantMenu = lazy( () => import("./src/components/RestaurantMenu") );
 // main div tag 
 const AppLayout = () =>
 {
@@ -42,8 +44,7 @@ const AppRouter = createBrowserRouter( [
             },
             {
                 path: "/Home" ,
-                element: <Body />,
-                errorElement: <Error />,
+                element: <Suspense fallback={<Shimmer/>}><Body /></Suspense>,
             },
             {
                 path: "/AboutUs",
@@ -61,7 +62,7 @@ const AppRouter = createBrowserRouter( [
             },
             {
                 path: "/restaurants/:resId",
-                element : <RestaurantMenu />
+                element : <Suspense fallback={<Shimmer/>} ><RestaurantMenu /></Suspense>
             },
         ],
         errorElement: <Error />,
