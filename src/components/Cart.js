113efdf147fc93d0d545b1cc66949f2ba28d48a7
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-03-07 19:49:21 +0530

    Added Remove Item button in Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
-import { clearCart } from "../utils/cartSlice";
+import { clearCart,removeItems } from "../utils/cartSlice";
 const Cart = () => {
     const cartItems = useSelector((store)=> store.cart.items); 
     const dispatch = useDispatch();
     const handleClearCart = () => {
         dispatch(clearCart())
     }
+    const handleRemoveCart = () => {
+        dispatch(removeItems());
+    }
     return (
-        <div>
+        <div >
             <h1 className="m-2 p-4 text-center text-2xl font-bold">Cart</h1>
-            <div className=" m-auto">
-                <button className="bg-black text-white rounded-md self-center  ml-[47.5%] p-4 " onClick={ () => handleClearCart()} >
+            <div className="flex-wrap flex-row " >
+                <button className="bg-black my-5 text-white rounded-md self-center w-36 ml-[46.5%] p-4  " onClick={ () => handleClearCart()} >
                     Clear items
+                </button>
+                <button className="bg-black text-white rounded-md self-center w-36 ml-[46.5%] p-4 " onClick={ () => handleRemoveCart()} >
+                    Remove item
                 </button>
             </div>
             { cartItems.length === 0 && <h1 className="m-2 p-4 text-center text-2xl font-bold" > Add to Cart </h1>  }
